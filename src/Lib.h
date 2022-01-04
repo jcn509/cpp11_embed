84c727a8d1146d086a3afd8e61fe3e4238322583
Josh Neil <joshneil8@googlemail.com>
2022-01-04 15:23:39 +0000

    First pass

---
+++ src/Lib.h
@@ -0,0 +1,9 @@
+#include <istream>
+#include <ostream>
+
+namespace cpp11embed
+{
+    void OutputEscapedCharacter(char c, std::ostream &out);
+
+    void OutputEscapedStringLiteral(std::istream &input_stream, std::ostream &output_stream);
+}
---
+++ src/Main.cpp
@@ -0,0 +1,35 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <memory>
+
+#include "Lib.h"
+
+int main(const int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        std::cerr << "Usage:\n ";
+        return EXIT_FAILURE;
+    }
+    const char *input_filename = argv[1];
+    const char *output_filename = argv[2];
+    // Use std::optional? Would require C++17?
+    // Read in binary mode so that we embed the file contents
+    // exactly as they are
+    const std::unique_ptr<std::ifstream> in_file_stream = (std::string{input_filename} == "-") ? nullptr : std::make_unique<std::ifstream>(input_filename, std::ifstream::binary);
+    const std::unique_ptr<std::ofstream> out_file_stream = (std::string{output_filename} == "-") ? nullptr : std::make_unique<std::ofstream>(output_filename, std::ifstream::binary);
+    //const bool binary_mode = false;
+    std::istream &input_stream = (in_file_stream == nullptr) ? std::cin : *in_file_stream;
+    std::ostream &output_stream = (out_file_stream == nullptr) ? std::cout : *out_file_stream;
+    if (input_stream)
+    {
+        cpp11embed::OutputEscapedStringLiteral(input_stream, output_stream);
+    }
+    else
+    {
+        // couldn't open the file
+    }
+
+    return EXIT_SUCCESS;
+}
