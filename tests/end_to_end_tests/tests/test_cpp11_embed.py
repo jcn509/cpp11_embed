e94ad8f9d75217ac18528c41596666bd45ae28e6
Josh Neil <joshneil8@googlemail.com>
2022-01-23 09:52:56 +0000

    Test help command

--- tests/end_to_end_tests/tests/test_cpp11_embed.py
+++ tests/end_to_end_tests/tests/test_cpp11_embed.py
@@ -68,6 +68,41 @@ def test_successful_text_file_input_output_to_stdout(
 @pytest.mark.parametrize(
     "file_name, expected_string_literal_contents", _FILES_AND_ESCAPED_CONTENTS
 )
+@pytest.mark.parametrize("help_flag", ("--help", "-h"))
+def test_help_flag(
+    identifier_name: str,
+    use_header_guard: bool,
+    file_name: str,
+    expected_string_literal_contents: str,
+    help_flag: str,
+):
+    """Ensure that when the help flag is used some help text is printed and
+    the app exits with code 0 without doing any work.
+
+    Also makes sure that nothing is written to standard error.
+    """
+    result = run_cpp11_embed(
+        _TEST_FILES_DIR / file_name,
+        identifier_name,
+        use_header_guard,
+        other_arguments=(help_flag,),
+    )
+    expected_header = _get_expected_text_data_header(
+        identifier_name, use_header_guard, expected_string_literal_contents
+    )
+    assert (
+        "Display this help menu" in result.stdout
+    ), "Help menu appears to have been displayed"
+    assert expected_header not in result.stdout, "No header produced"
+    assert result.stderr == "", "No errors reported"
+    assert result.returncode == 0, "No errors reported"
+
+
+@pytest.mark.parametrize("identifier_name", ("test_name", "other_name"))
+@pytest.mark.parametrize("use_header_guard", (True, False))
+@pytest.mark.parametrize(
+    "file_name, expected_string_literal_contents", _FILES_AND_ESCAPED_CONTENTS
+)
 def test_successful_text_stdin_output_to_stdout(
     identifier_name: str,
     use_header_guard: bool,
