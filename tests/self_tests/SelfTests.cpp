2f4bc101bc6c5dc98bb38b9d4342c24b7681ce35
Josh Neil <joshneil8@googlemail.com>
2022-01-23 12:52:27 +0000

    Add example with header in a sub directory

--- tests/self_tests/SelfTests.cpp
+++ tests/self_tests/SelfTests.cpp
@@ -7,18 +7,17 @@
 // Project
 #include "BinaryHeader.h"
 #include "BinaryHeaderWithHeaderGuard.h"
+#include "InASubDirectory/TextHeader2.h"
 #include "TextHeader.h"
 #include "TextHeaderWithHeaderGuard.h"
 
-// include the headers twice to make sure that header guards and pragmas are
+// Include the headers twice to make sure that header guards and pragmas are
 // done correctly. If they aren't compilation will fail due to multiple
 // definitions of the same constants
-// clang-format off
 #include "BinaryHeader.h"
 #include "BinaryHeaderWithHeaderGuard.h"
 #include "TextHeader.h"
 #include "TextHeaderWithHeaderGuard.h"
-// clang-format on
 
 TEST_CASE("cpp11embedtest auto-generated text header",
           "[cpp11embed][SelfTest]") {
@@ -43,4 +42,10 @@ TEST_CASE("cpp11embedtest auto-generated binary header with header guard",
   REQUIRE(k_binary_header_with_header_guard ==
           std::array<uint8_t, 18>{111, 110, 101, 32, 108, 105, 110, 101, 10,
                                   116, 119, 111, 32, 108, 105, 110, 101, 115});
+}
+
+TEST_CASE("cpp11embedtest auto-generated text header in a subdirectory",
+          "[cpp11embed][SelfTest]") {
+  REQUIRE(std::strcmp(k_text_header_in_a_subdirectory, "one line\ntwo lines") ==
+          0);
 }
